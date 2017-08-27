1915e191f2402e5101273b63b1d7f04ecec0e93c
LAIA\Laia <laia-89@hotmail.com>
2017-08-27 22:36:55 +0200

    Separate the AJAX call from the url and the response

--- js/SearchRepository.js
+++ js/SearchRepository.js
@@ -3,34 +3,42 @@ $('#searchRepoBtn, .result input[name="order"]').off('click').on('click', functi
   const searchTerm = $('#searchRepoInput').val();
   if (searchTerm) {
     const order = $('.result input[name="order"]:checked').val();
-    $.when(getRepoData(searchTerm, order)).then((data) => fillTable(data));
+    //$.when(getRepoData(searchTerm, order)).then((data) => fillTable(data));
+    initProces(searchTerm, order);
   }
 })
 
-// AJAX example (using XMLHttpRequest class)
-function getRepoData(searchTerm, order) {
-  var $deferred = $.Deferred();
+function initProces(searchTerm, order) {
   const githubReposUrl = 'https://api.github.com/search/repositories';
-  const url = githubReposUrl + '?q=' + searchTerm + '&per_page=20&order=' + order;
+  const url = githubReposUrl + '?q=' + searchTerm + '&per_page=10&order=' + order;
+
+  $.when(getRepoData(url))
+    .then(( data ) => {
+      const result = `Found ${data.total_count} repositories about ${searchTerm}`;
+      $('.result span').text(result);
+      fillTable(data);
+    },
+    ( status ) => $('.result span').text(`There was an error: ${status}`));
+  $('.result span').text("sending request with XMLHttpRequest...");
+};
+
+function getRepoData(url) {
+  var $deferred = $.Deferred();
   const xhr = new XMLHttpRequest();
   xhr.open("GET", url);
   xhr.onload = () => {
     console.log("AJAX request finished correctly :)");
     const data = JSON.parse(xhr.responseText); // converts response to JSON object
     $deferred.resolve(data);
-    const result = `Found ${data.total_count} repositories about ${searchTerm}`;
-    $('.result span').text(result);
   };
-
   xhr.onerror = () => {
     console.log("AJAX request finished with an error :(");
     $deferred.reject(xhr.statusText);
-    $('.result span').text(`There was an error: ${xhr.statusText}`);
   };
-  $('.result span').text("sending request with XMLHttpRequest...");
   xhr.send();
   return $deferred.promise();
 };
+
 
 function fillTable(data) {
   var htmlRow = '';
